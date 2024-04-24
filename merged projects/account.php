e1c2f571361999384d6aab210fc6c0ffb7b3fab3
MoroniStubbe <moronistubbe2000@gmail.com>
2024-04-24 14:27:20 +0200

    fix account page

--- merged projects/account.php
+++ merged projects/account.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>My Account</title>
-    <link rel="stylesheet" href="accountstyle.css">
+    <link rel="stylesheet" href="css/accountstyle.css">
 </head>
 <body>
 <nav id="navbar">
