3332c745564b12499c72e0260945220c024b292d
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-04-26 14:48:06 +0200

    add new database

--- merged projects/uneedit.sql
+++ merged projects/uneedit.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 24, 2024 at 02:37 PM
+-- Generation Time: Apr 26, 2024 at 02:46 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `school`
+-- Database: `uneedit`
 --
 
 -- --------------------------------------------------------
@@ -33,6 +33,13 @@ CREATE TABLE `adminpanel` (
   `ID` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `adminpanel`
+--
+
+INSERT INTO `adminpanel` (`User_name`, `Password`, `ID`) VALUES
+('uneedit', 'root', 162);
+
 -- --------------------------------------------------------
 
 --
@@ -44,6 +51,13 @@ CREATE TABLE `contact_info` (
   `ID` int(255) NOT NULL,
   `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `contact_info`
+--
+
+INSERT INTO `contact_info` (`telefoon_nummer`, `ID`, `email`) VALUES
+(0, 169, 'asd');
 
 -- --------------------------------------------------------
 
@@ -67,6 +81,13 @@ CREATE TABLE `nieuws` (
   `ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `nieuws`
+--
+
+INSERT INTO `nieuws` (`nieuwsbericht`, `ID`) VALUES
+('hallo lorem ipsum', 11);
+
 -- --------------------------------------------------------
 
 --
@@ -81,21 +102,12 @@ CREATE TABLE `reparatie_aanvraag` (
   `contact_ID` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `requests`
+-- Dumping data for table `reparatie_aanvraag`
 --
 
-CREATE TABLE `requests` (
-  `id` int(11) NOT NULL,
-  `idvanklant` int(11) DEFAULT NULL,
-  `typemachine` enum('Niet gespecificeerd','Apple','Computer','Tablet','Laptop','Smartphone','Tarieven computer','Tarieven Apple') DEFAULT NULL,
-  `garantie` enum('ja','nee') DEFAULT NULL,
-  `datum` date DEFAULT NULL,
-  `photo_path` varchar(255) DEFAULT NULL,
-  `omschrijving` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `reparatie_aanvraag` (`device_type`, `computer_naam`, `probleem`, `ID`, `contact_ID`) VALUES
+('telefoon', 'asd', 'asd', 196, 169);
 
 -- --------------------------------------------------------
 
@@ -112,6 +124,16 @@ CREATE TABLE `users` (
   `password` varchar(50) NOT NULL,
   `role` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `naam`, `telefoonnummer`, `email`, `address`, `password`, `role`) VALUES
+(1, 'Vova Hladkov', '0997729404', 'vovagladkov33@gmail.com', 'opopopo 34', 'admin007', 'klant'),
+(4, 'admin007', '9977294040', 'admin007@gmail.com', 'adminstraat 007', 'admin007', 'admin'),
+(7, 'klant007', '090909090909', 'klant@gmail.com', 'klantstraat 007', 'klant007', 'klant'),
+(8, 'asd', 'asd', 'asd@asd', 'asd', 'asd', 'klant');
 
 --
 -- Indexes for dumped tables
@@ -148,13 +170,6 @@ ALTER TABLE `reparatie_aanvraag`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indexes for table `requests`
---
-ALTER TABLE `requests`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `idvanklant` (`idvanklant`);
-
---
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -168,53 +183,37 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `adminpanel`
 --
 ALTER TABLE `adminpanel`
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=163;
 
 --
 -- AUTO_INCREMENT for table `contact_info`
 --
 ALTER TABLE `contact_info`
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=170;
 
 --
 -- AUTO_INCREMENT for table `faq`
 --
 ALTER TABLE `faq`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT for table `nieuws`
 --
 ALTER TABLE `nieuws`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `reparatie_aanvraag`
 --
 ALTER TABLE `reparatie_aanvraag`
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `requests`
---
-ALTER TABLE `requests`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=197;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `requests`
---
-ALTER TABLE `requests`
-  ADD CONSTRAINT `requests_ibfk_1` FOREIGN KEY (`idvanklant`) REFERENCES `users` (`id`);
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
