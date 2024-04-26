707c5cf8f055bfae260b8721dc08a4fb1db95d14
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-04-26 14:18:52 +0200

    fix nav html

--- merged projects/nieuws.php
+++ merged projects/nieuws.php
@@ -22,7 +22,7 @@
                         <li><a href="service.html">Service </a></li>
                         <li><a href="contact.html">Contact</a></li>
                         <li><a href="aanvraag.php">Aanvraag</a></li>
-                        <li><a href="faq.php">FAQ</a></li>
+                        <li><a href="faq.html">FAQ</a></li>
                         <li><a href="Bezorgdiensten.html">Bezorgdiensten</a></li>
                         <li><a href="account.php">Account</a>
                     </ul>
