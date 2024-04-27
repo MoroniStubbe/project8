aae679de5e507cc519ca6e242b5f2c818ec0bf1e
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-04-27 16:43:36 +0200

    rename files

--- merged projects/account.php
+++ merged projects/account.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>My Account</title>
-    <link rel="stylesheet" href="css/accountstyle.css">
+    <link rel="stylesheet" href="css/account.css">
     <link rel="stylesheet" href="css/nav.css">
 </head>
 
