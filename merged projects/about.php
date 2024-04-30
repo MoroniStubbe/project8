2361ed09c6f04b0fb46d9ec87f5b5a071c12f4d4
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-04-30 13:41:09 +0200

    fix about

--- merged projects/about.php
+++ merged projects/about.php
@@ -11,25 +11,25 @@
 
 <body>
     <?php readfile("header.html") ?>
-    <div class="background-container"></div>
-    <div class="main-content">
-        <section class="about">
+    <main>
+        <div id="background"></div>
+        <section id="about-container">
             <div class="container">
-                <div class="about">
+                <div id="about">
                     <h2>About UNEED-IT</h2>
-                    <p class>
+                    <p>
                         Welkom bij Uneed-it, uw vertrouwde partner voor al uw reparatiebehoeften. Bij Uneed-it streven
                         we ernaar hoogwaardige kwalitatieve reparatiediensten te bieden met een onwrikbare focus op
                         klanttevredenheid en excellentie in kwaliteit.
                     </p>
-                    <p class>
+                    <p>
                         Met een schat aan ervaring in de sector hebben we een onberispelijke reputatie opgebouwd als een
                         bedrijf dat synoniem staat voor vakmanschap, snelle service en eerlijke prijzen. Of het nu gaat
                         om het herstellen van elektronica, huishoudelijke apparaten, auto's of andere technische
                         apparaten, ons team van deskundige technici staat paraat om uw problemen op te lossen en uw
                         apparaten weer in optimale staat te herstellen.
                     </p>
-                    <p class>
+                    <p>
                         Bij Uneed-it streven we naar een naadloze ervaring, vanaf het moment dat u contact met ons
                         opneemt tot het moment dat u tevreden bent met de uitgevoerde reparatie. We begrijpen hoe
                         essentieel uw apparaten zijn voor uw dagelijks leven, en daarom doen we er alles aan om ze snel
@@ -43,7 +43,7 @@
                 <img src="img/person.jpg" alt="About Image" class="about-image">
             </div>
         </section>
-    </div>
+    </main>
     <?php readfile("footer.html") ?>
 </body>
 
