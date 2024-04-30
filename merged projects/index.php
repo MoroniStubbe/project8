0304ef5455bc15b9087d978557afbe828dea3c78
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-04-30 13:09:17 +0200

    fix index

--- merged projects/index.php
+++ merged projects/index.php
@@ -15,9 +15,7 @@
     <?php readfile("header.html") ?>
     <main>
         <section id="hero">
-            <div class="container">
-                <h1>Welcome to UNEED-IT</h1>
-            </div>
+            <h1>Welcome to UNEED-IT</h1>
         </section>
         <section id="services">
             <div class="container">
@@ -36,9 +34,9 @@
                 </div>
             </div>
         </section>
-        <section class="iframe">
+        <section id="iframe">
             <div class="container">
-                <div class="location">
+                <div id="location">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2455.469405079923!2d4.6556065!3d52.0165458!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5d125952a58cd%3A0x93f677d7fe6faaae!2sUneed-IT!5e0!3m2!1sen!2snl!4v1712919752830!5m2!1sen!2snl" width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
                 </div>
             </div>
