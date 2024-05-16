32787cf1e50d870b91d1ab03e025fd4ff0095d4b
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-05-16 17:40:17 +0200

    add create

--- merged projects/classes/repair_request.php
+++ merged projects/classes/repair_request.php
@@ -5,9 +5,34 @@ class RepairRequest
     private $db;
     private $id;
     private $device_type;
-    private $computer_naam;
-    private $probleem;
-    private $telefoon_nummer;
+    private $device_name;
+    private $problem;
+    private $telephone;
     private $email;
 
+    function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    function create($device_type, $device_name, $problem, $telephone, $email)
+    {
+        $this->device_type = $device_type;
+        $this->device_name = $device_name;
+        $this->problem = $problem;
+        $this->telephone = $telephone;
+        $this->email = $email;
+
+        $this->db->create("repair_request", [
+            "device_type" => $device_type,
+            "device_name" => $device_name,
+            "problem" => $problem,
+            "telephone" => $telephone,
+            "email" => $email
+        ]);
+    }
+
+    function read()
+    {
+    }
 }
