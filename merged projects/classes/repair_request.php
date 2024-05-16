73ae830e8bc70c6ac83dc0a559187156915588e7
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-05-16 17:31:26 +0200

    add RepairRequest class

---
+++ merged projects/classes/repair_request.php
@@ -0,0 +1,13 @@
+<?php
+
+class RepairRequest
+{
+    private $db;
+    private $id;
+    private $device_type;
+    private $computer_naam;
+    private $probleem;
+    private $telefoon_nummer;
+    private $email;
+
+}
