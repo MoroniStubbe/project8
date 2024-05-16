a34f96d46043c3e2076b77be468bc748aae833cb
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-05-16 18:05:34 +0200

    add read

--- merged projects/classes/repair_request.php
+++ merged projects/classes/repair_request.php
@@ -3,6 +3,7 @@
 class RepairRequest
 {
     private $db;
+    private $table = "repair_request";
     private $id;
     private $device_type;
     private $device_name;
@@ -23,7 +24,7 @@ class RepairRequest
         $this->telephone = $telephone;
         $this->email = $email;
 
-        $this->db->create("repair_request", [
+        $this->db->create($this->table, [
             "device_type" => $device_type,
             "device_name" => $device_name,
             "problem" => $problem,
@@ -32,7 +33,28 @@ class RepairRequest
         ]);
     }
 
-    function read()
+    function read($id = null)
     {
+        if (!$id) {
+            if (!$this->id) {
+                return "no id provided";
+            }
+
+            $id = $this->$id;
+        } else {
+            $this->id = $id;
+        }
+
+        $result = $this->db->read($this->table, where: ["id" => $id])[0];
+
+        if (gettype($result) == "string") {
+            return $result;
+        }
+
+        $this->device_type = $result["device_type"];
+        $this->device_name = $result["device_name"];
+        $this->problem = $result["problem"];
+        $this->telephone = $result["telephone"];
+        $this->email = $result["email"];
     }
 }
--- merged projects/tests/repair_request.php
+++ merged projects/tests/repair_request.php
@@ -9,3 +9,6 @@ include_once("../classes/repair_request.php");
 $db = new Database($PDO);
 $repair_request = new RepairRequest($db);
 $repair_request->create("device_type", "device_name", "problem", "telephone", "email");
+$repair_request->read(199);
+
+$a = 0;
