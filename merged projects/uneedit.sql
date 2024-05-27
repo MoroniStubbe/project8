c054cd85abdab1b30bb71ecfcdb4bc5008bd6995
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-05-27 18:01:10 +0200

    update database password_hash

--- merged projects/uneedit.sql
+++ merged projects/uneedit.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 27, 2024 at 04:54 PM
+-- Generation Time: May 27, 2024 at 06:00 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -33,7 +33,7 @@ CREATE TABLE `accounts` (
   `phone` varchar(15) NOT NULL,
   `email` varchar(50) NOT NULL,
   `address` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password_hash` char(60) NOT NULL,
   `role` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -41,7 +41,7 @@ CREATE TABLE `accounts` (
 -- Dumping data for table `accounts`
 --
 
-INSERT INTO `accounts` (`id`, `name`, `phone`, `email`, `address`, `password`, `role`) VALUES
+INSERT INTO `accounts` (`id`, `name`, `phone`, `email`, `address`, `password_hash`, `role`) VALUES
 (20, 'asd2', 'asd', 'asd@asd', 'asd', 'asd', 'customer');
 
 -- --------------------------------------------------------
