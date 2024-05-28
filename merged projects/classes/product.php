15c45ec029b7e0914780aace712207f281e58804
MoroniStubbe <moronistubbe2000@gmail.com>
2024-05-28 15:20:34 +0200

    add class skeleton

---
+++ merged projects/classes/product.php
@@ -0,0 +1,29 @@
+<?php
+
+class Product
+{
+    private $id;
+    private $name;
+    private $description;
+    private $image_url;
+    private $price;
+    private $stock;
+    private $shopping_cart;
+    private $category;
+
+    function __construct()
+    {
+    }
+
+    private function update()
+    {
+    }
+
+    private function read()
+    {
+    }
+
+    private function inflate_price()
+    {
+    }
+}
