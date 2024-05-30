b84c1248726cdab0dae42bae4da216be1226e862
Jiwoo12345 <yahootomb370@gmail.com>
2024-05-30 14:35:29 +0200

    add shopping cart class skeleton

---
+++ merged projects/classes/shopping_cart.php
@@ -0,0 +1,39 @@
+<?php
+
+class Shopping_cart
+{
+    private $db;
+    public $id;
+    public $name;
+    public $products;
+
+    function __construct()
+    {
+
+    }
+
+    private function read()
+    {
+
+    }
+
+    private function delete()
+    {
+
+    }
+
+    private function add_product()
+    {
+
+    }
+
+    private function summary()
+    {
+        
+    }
+    
+    private function pay_product()
+    {
+        
+    }
+}
