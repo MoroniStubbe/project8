fffb1ad4d37c4cc84545334a9e2651e17f27f305
Aidan De Groot <aidanjdegroot@gmail.com>
2024-06-10 12:23:26 +0200

    add javascript to faq.php

--- merged projects/faq.php
+++ merged projects/faq.php
@@ -34,4 +34,23 @@
 <?php readfile("footer.html") ?>
 </body>
 
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        const questions = document.querySelectorAll(".question");
+
+        questions.forEach(question => {
+            question.addEventListener("click", function() {
+                const answer = this.nextElementSibling;
+                if (answer.style.display === "block") {
+                    answer.style.display = "none";
+                } else {
+                    answer.style.display = "block";
+                }
+            });
+        });
+    });
+</script>
+
+
+
 </html>
