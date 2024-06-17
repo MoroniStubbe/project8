87999a398f844e4e67042e002b9384c2addad16f
Jiwoo12345 <yahootomb370@gmail.com>
2024-06-17 13:50:03 +0200

    repair request migration

---
+++ uneedit-laravel/database/migrations/2024_06_17_114439_create_repair_request_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('repair_request', function (Blueprint $table) {
+            $table->id();
+            $table->string("device_type");
+            $table->string("device_name");
+            $table->string("problem");
+            $table->string("telephone");
+            $table->string("email");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('repair_request');
+    }
+};
