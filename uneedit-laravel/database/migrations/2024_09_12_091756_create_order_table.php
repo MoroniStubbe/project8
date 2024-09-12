8653d6c86fddeb2cf1009d52ac78848c2d574f6e
Jiwoo12345 <yahootomb370@gmail.com>
2024-09-12 11:31:42 +0200

    order table migration (not complete)

--- uneedit-laravel/database/migrations/2024_09_12_091756_create_order_table.php
+++ uneedit-laravel/database/migrations/2024_09_12_091756_create_order_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('order', function (Blueprint $table) {
             $table->id();
+            $table->string("user_id");
+            // $table->string("product");
+            $table->string("price");
             $table->timestamps();
         });
     }
