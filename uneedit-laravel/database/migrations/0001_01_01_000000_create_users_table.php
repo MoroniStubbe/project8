a23e5e1afe3d6dd84147bc35be5c0554b884353f
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-09-12 11:18:58 +0200

    update users migration

--- uneedit-laravel/database/migrations/0001_01_01_000000_create_users_table.php
+++ uneedit-laravel/database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,9 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('phone');
             $table->string('address');
-            $table->string('role')->default('user');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_admin');
             $table->rememberToken();
             $table->timestamps();
         });
