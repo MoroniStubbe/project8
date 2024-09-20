77bb58f2224cd41a4f84364fad9e5ced1c1bf8c2
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-09-20 09:43:04 +0200

    Update add_product.blade.php

--- uneedit-laravel/resources/views/admin/add_product.blade.php
+++ uneedit-laravel/resources/views/admin/add_product.blade.php
@@ -1,40 +1,40 @@
 <html>
 
 <head>
-    <link rel="stylesheet" href="{{ asset('css/add_user.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/admin_panel.css') }}">
 </head>
 
 <body>
     <x-admin_nav></x-admin_nav>
     <main>
-    <form action="{{ route('admin.add.product') }}" method="POST">
-        @csrf
-        <label for="type">Type:</label>
-        <input type="text" name="type" value="{{ old('type') }}">
-        <br>
+        <form action="{{ route('admin.add.product') }}" method="POST">
+            @csrf
+            <label for="type">Type:</label>
+            <input type="text" name="type" value="{{ old('type') }}">
+            <br>
 
-        <label for="name">Name:</label>
-        <input type="text" name="name" value="{{ old('name') }}">
-        <br>
+            <label for="name">Name:</label>
+            <input type="text" name="name" value="{{ old('name') }}">
+            <br>
 
-        <label for="price">Price:</label>
-        <input type="number" step="0.01" name="price" value="{{ old('price') }}">
-        <br>
+            <label for="price">Price:</label>
+            <input type="number" step="0.01" name="price" value="{{ old('price') }}">
+            <br>
 
-        <label for="picture">Picture:</label>
-        <input type="text" name="picture" value="{{ old('picture') }}">
-        <br>
+            <label for="picture">Picture:</label>
+            <input type="text" name="picture" value="{{ old('picture') }}">
+            <br>
 
-        <label for="stock">Stock:</label>
-        <input type="number" name="stock" value="{{ old('stock') }}">
-        <br>
+            <label for="stock">Stock:</label>
+            <input type="number" name="stock" value="{{ old('stock') }}">
+            <br>
 
-        <label for="description">Description:</label>
-        <textarea name="description">{{ old('description') }}</textarea>
-        <br>
+            <label for="description">Description:</label>
+            <textarea name="description">{{ old('description') }}</textarea>
+            <br>
 
-        <button type="submit">Submit</button>
-    </form>
+            <button type="submit">Submit</button>
+        </form>
     </main>
 </body>
 
