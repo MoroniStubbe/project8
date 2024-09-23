a3af2a7c1b00031c9f7155df7a65404346b9575f
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-09-23 12:50:45 +0200

    Update FormController.php

--- uneedit-laravel/app/Http/Controllers/FormController.php
+++ uneedit-laravel/app/Http/Controllers/FormController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class FormController extends Controller
 {
-    //
+    public static function print_table($data)
+    {
+        echo "<form>";
+        echo csrf_field();
+        echo "<table class='table1'><tr>";
+        foreach (array_keys($data[0]) as $key) {
+            echo "<th>" . $key . "</th>";
+        }
+        echo "</tr>";
+
+        foreach ($data as $row) {
+            echo "<tr>";
+            foreach ($row as $col) {
+                echo "<td>" . $col . "</td>";
+            }
+            echo "</tr>";
+        }
+        echo "</table></form>";
+    }
 }
