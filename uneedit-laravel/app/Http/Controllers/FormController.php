6ca36ea4381c1828dca791a40e1bca1cd12b78ac
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-09-26 11:58:25 +0200

    Update FormController.php

--- uneedit-laravel/app/Http/Controllers/FormController.php
+++ uneedit-laravel/app/Http/Controllers/FormController.php
@@ -8,23 +8,34 @@ class FormController extends Controller
 {
     public static function print_table($data)
     {
-        if (count($data)) {
-            echo "<form>";
-            echo csrf_field();
-            echo "<table class='table1'><tr>";
-            foreach (array_keys($data[0]) as $key) {
-                echo "<th>" . $key . "</th>";
-            }
-            echo "</tr>";
-
-            foreach ($data as $row) {
-                echo "<tr>";
-                foreach ($row as $col) {
-                    echo "<td>" . $col . "</td>";
-                }
-                echo "</tr>";
-            }
-            echo "</table></form>";
+        if (!count($data)) {
+            return;
         }
+
+        echo "<form>";
+        echo csrf_field();
+        echo "<table class='table1'>";
+        echo "<tr>";
+
+        //headers
+        foreach (array_keys($data[0]) as $key) {
+            echo "<th>" . $key . "</th>";
+        }
+
+        echo "</tr>";
+
+        //rows
+        foreach ($data as $row) {
+            echo "<tr>";
+
+            foreach ($row as $col) {
+                echo "<td>" . $col . "</td>";
+            }
+
+            echo "</tr>";
+        }
+
+        echo "</table>";
+        echo "</form>";
     }
 }
