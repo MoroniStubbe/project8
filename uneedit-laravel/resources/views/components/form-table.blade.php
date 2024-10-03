6c8923e33edc7b9e6f9b11dfc2e308f5bfdbf855
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-10-03 11:23:25 +0200

    Update form-table.blade.php

--- uneedit-laravel/resources/views/components/form-table.blade.php
+++ uneedit-laravel/resources/views/components/form-table.blade.php
@@ -1,3 +1,27 @@
-<div>
-    <!-- He who is contented is rich. - Laozi -->
-</div>
+<form method="POST" action="{{ $action }}">
+    @csrf
+    <button type="submit" id="createButton">Create</button>
+
+    <table class="table1">
+        <thead>
+            <tr>
+                @foreach(array_keys($data[0]) as $header)
+                <th>{{ $header }}</th>
+                @endforeach
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($data as $row)
+            <tr>
+                @foreach($row as $value)
+                <td>{{ $value }}</td>
+                @endforeach
+                <td><button type="button">Edit</button></td>
+                <td><button type="button">Delete</button></td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</form>
