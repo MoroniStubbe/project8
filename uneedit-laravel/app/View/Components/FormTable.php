438178f786dc2c0ae33b198b2e830e0af5c0d39f
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-10-03 13:55:50 +0200

    Update FormTable.php

--- uneedit-laravel/app/View/Components/FormTable.php
+++ uneedit-laravel/app/View/Components/FormTable.php
@@ -8,19 +8,19 @@ use Illuminate\View\Component;
 
 class FormTable extends Component
 {
-    public $data;
+    public $table_data;
     public $action; // Add action variable
 
     /**
      * Create a new component instance.
      *
-     * @param array $data
+     * @param array $table_data
      * @param string $action
      * @return void
      */
-    public function __construct($data, $action)
+    public function __construct($tableData, $action)
     {
-        $this->data = $data;
+        $this->table_data = $tableData;
         $this->action = $action; // Assign action variable
     }
 
