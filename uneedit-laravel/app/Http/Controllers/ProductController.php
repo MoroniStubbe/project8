4684e63ce5cc08be57756e46c7b484af6a127537
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-10-03 13:55:48 +0200

    Update ProductController.php

--- uneedit-laravel/app/Http/Controllers/ProductController.php
+++ uneedit-laravel/app/Http/Controllers/ProductController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function show()
+    {
+        // Retrieve all products and convert them to an array
+        $table_data = Product::all()->toArray();
+
+        // Define the action URL for the form submission
+        $action = route('index'); // @TODO Update this route name
+
+        // Return the view with the products table_data
+        return view('admin.add_product', compact('table_data', 'action'));
+    }
 }
