9aa6464abc387f5e08b39787ecb7ab902bffc7bb
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-10-03 11:46:48 +0200

    Update add_product.blade.php

--- uneedit-laravel/resources/views/admin/add_product.blade.php
+++ uneedit-laravel/resources/views/admin/add_product.blade.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Models\Product;
-use App\Http\Controllers\FormController;
 
 ?>
 
@@ -14,61 +13,7 @@ use App\Http\Controllers\FormController;
 <body>
     <x-admin_nav></x-admin_nav>
     <main>
-        <?php FormController::print_table(Product::all()->toArray()); ?>
-        <form action="{{ route('admin.add.product') }}" method="POST">
-            @csrf
-            <table>
-                <tr>
-                    <td>
-                        <label for="type">Type</label>
-                    </td>
-                    <td>
-                        <input type="text" name="type" value="{{ old('type') }}">
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label for="name">Name</label>
-                    </td>
-                    <td>
-                        <input type="text" name="name" value="{{ old('name') }}">
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label for="price">Price</label>
-                    </td>
-                    <td>
-                        <input type="number" step="0.01" name="price" value="{{ old('price') }}">
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label for="picture">Picture</label>
-                    </td>
-                    <td>
-                        <input type="text" name="picture" value="{{ old('picture') }}">
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label for="stock">Stock</label>
-                    </td>
-                    <td>
-                        <input type="number" name="stock" value="{{ old('stock') }}">
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label for="description">Description</label>
-                    </td>
-                    <td>
-                        <textarea name="description">{{ old('description') }}</textarea>
-                    </td>
-                </tr>
-            </table>
-            <button type="submit">Submit</button>
-        </form>
+        <x-form-table :data="Product::all()->toArray()" action="{{ route('admin-form-submit') }}">
     </main>
 </body>
 
