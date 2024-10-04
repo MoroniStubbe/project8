faa921cfe053cfa0afd63608796e4f55d6c7415b
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-10-04 14:35:16 +0200

    Update add_product.blade.php

--- uneedit-laravel/resources/views/admin/add_product.blade.php
+++ uneedit-laravel/resources/views/admin/add_product.blade.php
@@ -14,7 +14,8 @@ use App\Models\Product;
 <body>
     <x-admin_nav></x-admin_nav>
     <main>
-        <x-form-table :tableData="$table_data" :action="$action" />
+        <x-form-table id="form_1" :tableData="$table_data" :action="$action" />
+        <x-form-table id="form_2" :tableData="$table_data" :action="$action" />
     </main>
 </body>
 
