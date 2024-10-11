43c9094679c3e5e9cdc76b8b8ee5ef16e0702ca6
Jiwoo12345 <yahootomb370@gmail.com>
2024-10-11 13:27:43 +0200

    Relate order table and order junction table

--- uneedit-laravel/database/migrations/2024_10_11_112502_create_order_table.php
+++ uneedit-laravel/database/migrations/2024_10_11_112502_create_order_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('order', function (Blueprint $table) {
             $table->id();
-            $table->integer("user_id");
+            $table->foreignId("order_product_junction_id")->constrained("order_product_junction");
             $table->timestamps();
         });
     }
