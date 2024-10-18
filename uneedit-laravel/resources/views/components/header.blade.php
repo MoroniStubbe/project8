8ac48d3b3078d3971766444ad703c066b42f131c
Kevin van Hal <kevinvhal2005@gmail.com>
2024-10-18 12:29:40 +0200

    header fix / update

--- uneedit-laravel/resources/views/components/header.blade.php
+++ uneedit-laravel/resources/views/components/header.blade.php
@@ -12,7 +12,7 @@
                 <li><a href="{{route('contact')}}">Contact</a></li>
                 <li><a href="{{route('request.view')}}">Aanvraag</a></li>
                 <li><a href="{{route('faq')}}">FAQ</a></li>
-                <li><a href="{{route('delivery_services')}}">Bezorgdiensten</a></li>
+                <li><a href="{{route('webshop.index')}}">Webshop</a></li>
                 <li><a href="{{ route('login_or_signup') }}"><img src="{{ asset('img/account_white.png') }}" alt="account"></a></li>
             </ul>
         </nav>
