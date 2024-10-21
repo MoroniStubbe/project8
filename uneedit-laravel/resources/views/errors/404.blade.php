de7e5c0a52376a4551544874e2eabcd6f6571e79
Kevin van Hal <kevinvhal2005@gmail.com>
2024-10-21 13:37:55 +0200

    404 error page

---
+++ uneedit-laravel/resources/views/errors/404.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 Not Found</title>
+    <link rel="stylesheet" href="{{ asset('css/error.css') }}">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;500&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <h1>404</h1>
+        <p>Oops! The page you're looking for doesn't exist.</p>
+        <a href="{{ route('index') }}" class="link">← Go back home</a>
+    </div>
+</body>
+</html>
