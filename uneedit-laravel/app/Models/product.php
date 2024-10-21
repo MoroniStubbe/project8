9687f35e33cc89799f48b2603aaf3cdcbf39cae3
Jiwoo12345 <yahootomb370@gmail.com>
2024-10-21 14:41:14 +0200

    Update product model

--- uneedit-laravel/app/Models/product.php
+++ uneedit-laravel/app/Models/product.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['type', 'name', 'price', 'picture', 'stock', 'description'];
+
+    // Relationship to the product-order junction
+    public function junction()
+    {
+        return $this->hasMany(OrderProductJunction::class);
+    }
 }
