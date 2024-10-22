66a097d0bbe0ad8061915a307e5ba997d993355b
Jiwoo12345 <yahootomb370@gmail.com>
2024-10-22 14:21:53 +0200

    Update order migration

--- uneedit-laravel/database/migrations/2024_10_11_112502_create_order_table.php
+++ uneedit-laravel/database/migrations/2024_10_11_112502_create_order_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignId("order_product_junction_id")->constrained('order_product_junction'); 
             $table->timestamps();
         });
     }
