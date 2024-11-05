271320334106203216d96b319317a185c782ddb1
Aidan De Groot <aidanjdegroot@gmail.com>
2024-11-05 11:42:51 +0100

    update _products.blade.php

--- uneedit-laravel/resources/views/webshop/_products.blade.php
+++ uneedit-laravel/resources/views/webshop/_products.blade.php
@@ -1,12 +1,3 @@
 @foreach ($products as $product)
-    <div class="product">
-        <a href="{{ url('/webshop/product/' . $product->id) }}" class="product-link">
-            <img src="{{ asset('img/products/' . $product->picture) }}" alt="{{ $product->name }}">
-            <div class="product-info">
-                <h3>{{ $product->name }}</h3>
-                <p>€{{ number_format($product->price, 2) }}</p>
-            </div>
-        </a>
-    </div>
-    @endforeach
-
+    <x-product :product="$product" />
+@endforeach
