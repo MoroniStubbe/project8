a7c0cebea2aff2bfeecd2f74740420977848a753
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-11-06 11:04:08 +0100

    Update paypal.blade.php

--- uneedit-laravel/resources/views/webshop/paypal.blade.php
+++ uneedit-laravel/resources/views/webshop/paypal.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>PayPal Sandbox Example</title>
     <script src="https://www.paypal.com/sdk/js?client-id=Ac5uidI7YJlAm5oa9gjN6WYUqw7XGdurKvb34tdwUnVxSkGlCWee6fnOAT5S5e-8dc-m4iTlfduv-5aa&currency=EUR"></script>
 </head>
@@ -24,7 +25,18 @@
             },
             onApprove: function(data, actions) {
                 return actions.order.capture().then(function(details) {
-                    window.location.href = "http://localhost/project8/uneedit-laravel/public/webshop/";
+                    var xhr = new XMLHttpRequest();
+                    xhr.open("DELETE", "{{route('cart.forget');}}", true);
+                    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+                    xhr.setRequestHeader("X-CSRF-TOKEN", document.querySelector('meta[name="csrf-token"]').getAttribute('content'));
+
+                    xhr.onload = function() {
+                        if (xhr.status >= 200 && xhr.status < 300) {
+                            window.location.href = "http://localhost/project8/uneedit-laravel/public/webshop/";
+                        }
+                    };
+
+                    xhr.send();
                 });
             }
         }).render('#paypal-button-container');
