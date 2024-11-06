8fb52c3d187992ce250deb45ce9dd92159ded730
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-11-06 11:33:29 +0100

    Update admin_nav.blade.php

--- uneedit-laravel/resources/views/components/admin_nav.blade.php
+++ uneedit-laravel/resources/views/components/admin_nav.blade.php
@@ -2,11 +2,12 @@
     <nav class="nav">
         <img src="{{ asset('img/logo.png') }}" alt="Company Logo" class="logo">
         <ul>
-            <li><a href="{{ route('admin.add.user.view') }}">gebruiker toevoegen</a></li>
+            <li><a href="{{ route('admin.add.user.view') }}">gebruikers</a></li>
+            <li><a href="{{ route('admin.requests.view') }}">aanvragen</a></li>
+            <li><a href="{{ route('admin.orders.view') }}">bestellingen</a></li>
+            <li><a href="{{ route('admin.products.view') }}">producten</a></li>
             <li><a href="{{ route('admin.news.view') }}">nieuws</a></li>
-            <li><a href="{{ route('admin.requests.view') }}">aanvragen inzien</a></li>
             <li><a href="{{ route('admin.faq.panel.view') }}">FAQ</a></li>
-            <li><a href="{{ route('admin.products.view') }}">product toevoegen</a></li>
         </ul>
     </nav>
 </header>
